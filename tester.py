a7c228fecdebe1b33bda4eca489e0ace7ee06c06
gregory goren <grggoren@gmail.com>
2017-02-13 18:27:11 +0200

    minor bug fix

--- tester.py
+++ tester.py
@@ -2,4 +2,4 @@ import cross_validator as cv
 
 if __name__=="__main__":
     cross_validator = cv.cross_validator(5,"features",200)
-    cross_validator.k_fold_cross_validation()
+    cross_validator.k_fold_cross_validation_lambdamart()
