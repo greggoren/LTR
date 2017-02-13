2cefc81dbba3d6710e1344fc5925c35aa725fb88
gregory goren <grggoren@gmail.com>
2017-02-13 14:03:12 +0200

    test creation for ieir24

--- tester.py
+++ tester.py
@@ -1,5 +1,5 @@
 import cross_validator as cv
 
 if __name__=="__main__":
-    cross_validator = cv.cross_validator(5,"/features",200)
+    cross_validator = cv.cross_validator(5,"features",200)
     cross_validator.k_fold_cross_validation()
