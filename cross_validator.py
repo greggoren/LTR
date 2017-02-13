c9817a97e626852a189529e107a50184024ab29a
gregory goren <grggoren@gmail.com>
2017-02-13 13:05:24 +0200

    "creating result lists for experiment"

--- cross_validator.py
+++ cross_validator.py
@@ -8,8 +8,10 @@ class cross_validator:
         self.folds_creator = fc.folds_creator(k,train_data,number_of_queries,fold_prefix)
         self.folds_creator.split_train_file_into_folds()
         self.fold_prefix = fold_prefix
-        self.k = k
-
+        self.k =k
+        self.number_of_trees_for_test = [500, 250]
+        self.number_of_leaves_for_test = [5, 10]
+        self.test_metric = ["NDCG@20","P@10","P@5"]
 
     def create_train_set_for_ltr(self,test,validation):
         folds = self.folds_creator.folds
@@ -24,7 +26,7 @@ class cross_validator:
             os.remove(absolute_path)
         file_for_ltr = open('train.txt', 'w')
         for train_data in train_set:
-            file_for_ltr.write("%s\n" % train_data)
+            file_for_ltr.write("%s" % train_data)
         file_for_ltr.close()
 
 
@@ -37,7 +39,7 @@ class cross_validator:
             os.remove(absolute_path)
         file_for_ltr = open(absolute_path, 'w')
         for validation_data in validation_set:
-            file_for_ltr.write("%s\n" % validation_data)
+            file_for_ltr.write("%s" % validation_data)
         file_for_ltr.close()
 
     def create_test_set_for_ltr(self, test):
@@ -49,7 +51,7 @@ class cross_validator:
             os.remove(absolute_path)
         file_for_ltr = open(absolute_path, 'w')
         for test_data in test_set:
-            file_for_ltr.write("%s\n" % test_data)
+            file_for_ltr.write("%s" % test_data)
         file_for_ltr.close()
 
     def run_command(self,command):
@@ -58,18 +60,33 @@ class cross_validator:
                              stderr=subprocess.STDOUT)
         return iter(p.stdout.readline, b'')
 
-    def run_cross_validation_with_lambda_mart(self, test_fold, validation_fold):#TODO: create a dynamic environment for CV
-        #test_fold = self.k
-        #validation_fold = self.k-1
+    def run_cross_validation_with_lambda_mart(self, test_fold, validation_fold,number_of_trees,number_of_leaves,metric):
+
         prefix = self.folds_creator.fold_prefix
         self.create_train_set_for_ltr(prefix+str(test_fold),prefix+str(validation_fold))
         self.create_validation_set_for_ltr(prefix+str(validation_fold))
         self.create_test_set_for_ltr(prefix+str(test_fold))
-        #subprocess.call(['java','-jar','rlib.jar','-train train.txt','-test test.txt','-validate validation.txt','-ranker 6','-metric2t NDCG@10','-metric2T NDCG@10','myModel.txt'],shell=True)
-        command = 'java -jar rlib.jar -train train.txt -test test.txt -validate validation.txt -ranker 6 -metric2t NDCG@10 -metric2T NDCG@10 -save myModel'+str(test_fold)+'.txt'
+        command = 'java -jar RankLib-2.3.jar -train train.txt -test test.txt' \
+                  ' -validate validation.txt -ranker 6 -metric2t NDCG@20 -metric2T '+metric+' ' \
+                  '-tree '+str(number_of_trees) +' -leaf '+str(number_of_leaves) #+' -save myModel'+str(test_fold)+'.txt'
         for output_line in self.run_command(command):
             print(output_line)
+            if "on test data:" in str(output_line):
+                score = str(output_line).split("on test data:")[1]
+                return score
 
     def k_fold_cross_validation(self):
-        for phase in range(3,self.k+1):
-            self.run_cross_validation_with_lambda_mart(phase, phase - 1)
+        results_file = open("results.txt",'w')
+        results_file.write("#trees\t#leaves\tscore\tmetric\n")
+        for metric in self.test_metric:
+            for number_of_trees in self.number_of_trees_for_test:
+                for number_of_leaves in self.number_of_leaves_for_test:
+                    mean_score = 0
+                    for phase in range(2,self.k+1):
+                        score = self.run_cross_validation_with_lambda_mart(phase, phase - 1,number_of_trees,number_of_leaves,metric)
+                        score = score.split('\\r\\n')[0]
+                        mean_score += float(score)
+                    mean_score += float(self.run_cross_validation_with_lambda_mart(1, 3,number_of_trees,number_of_leaves,metric).split('\\r\\n')[0])
+                    mean_score = mean_score/self.k
+                    results_file.write(str(number_of_trees)+"\t"+str(number_of_leaves)+"\t"+str(mean_score)+"\t"+metric+"\n")
+        results_file.close()
