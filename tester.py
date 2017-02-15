4bb60d984d650c56c00947c8e8f58b5a50678323
gregory goren <grggoren@gmail.com>
2017-02-15 22:35:38 +0200

    test

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 import cross_validator as cv
 
 if __name__=="__main__":
-    cross_validator = cv.cross_validator(5,"features.txt","CBW08",200)
+    cross_validator = cv.cross_validator(5,"features","CBW08",200)
     cross_validator.k_fold_cross_validation("LAMBDAMART")
     #cross_validator.k_fold_cross_validation_lambdamart()
