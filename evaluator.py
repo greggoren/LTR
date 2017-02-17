5bef6e5ae06daeac0224ff2beba1924a296d1dba
gregory goren <grggoren@gmail.com>
2017-02-17 20:22:27 +0200

    test

--- evaluator.py
+++ evaluator.py
@@ -47,7 +47,7 @@ class evaluator:
 
                 trec_eval_formatted_file_before_sort_file.write(query_id+"\tQ0\t"+document_name+"\t"+str(row_number)+"\t"+str(score)+"\tindri\n")
                 row_number += 1
-                trec_eval_formatted_file_before_sort_file.close()
+            trec_eval_formatted_file_before_sort_file.close()
             command = "sort -k1,1 -k5 < "+trec_eval_formatted_file_before_sort+" > "+trec_eval_formatted_file_final
             for output_line in self.run_command(command):
                 print(output_line)
