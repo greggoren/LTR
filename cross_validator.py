9213103931b077c90ae2d3ac36652101ea692cf5
gregory goren <grggoren@gmail.com>
2017-02-18 15:12:13 +0200

    minor bug fix

--- cross_validator.py
+++ cross_validator.py
@@ -109,7 +109,7 @@ class cross_validator:
 
     def svm_models_creator(self,train_file,models_directory):
         for c_value in self.svm_c_params_to_test:
-            learning_command = "svm_rank_learn.exe -c " + str(c_value) + " "+train_file+" "+models_directory+"/svm_model"+str(c_value)+".txt"
+            learning_command = "svm_rank_learn -c " + str(c_value) + " "+train_file+" "+models_directory+"/svm_model"+str(c_value)+".txt"
             for output_line in self.run_command(learning_command):
                 print(output_line)
 
