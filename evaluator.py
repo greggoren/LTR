ea9b2219e2ca565aea7f5e67f7e034e5e161cc4d
gregory goren <grggoren@gmail.com>
2017-02-19 23:53:48 +0200

    test

--- evaluator.py
+++ evaluator.py
@@ -64,12 +64,12 @@ class evaluator:
                     final_score_file = score_dir[0]+"/"+score_file
                     command="./trec_eval -m "+self.evaluation_metric+ " "+qrel_path+" "+final_score_file
                     for output_line in self.run_command(command):
-                        evaluation_score = output_line.split()[-1].split()[0]
+                        evaluation_score = output_line.split()[-1]
                         print(evaluation_score)
 
                         scores.append((final_score_file,evaluation_score))
                         if max_score == 0.0:
-                            max_score = evaluation_score
+                            max_score = float(evaluation_score)
                             self.chosen_model = os.path.basename(final_score_file)
 
                         else:
