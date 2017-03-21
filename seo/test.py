1f93bde6e8c3db9cf68e3751113ab98856039a8f
gregory goren <grggoren@gmail.com>
2017-03-21 11:58:03 +0200

    fix

--- seo/test.py
+++ seo/test.py
@@ -7,13 +7,14 @@ import sys
 if __name__ == "__main__":
     data_set_location = sys.argv[0]
     final_path = sys.argv[1]
+    qrel_path = sys.argv[2]
     g = e.exponential_budget_cost_creator()
     q = qtf.qtf(data_set_location)
     q.create_query_to_fold_index()
     l = lfc.letor_folds_creator(data_set_location,data_set_location,q,True)
     o = odsc.files_rewriter()
     c = cv.cross_validator(5,"",l,"LTOR")
-    c.k_fold_cross_validation("SVM", "")
+    c.k_fold_cross_validation("SVM",qrel_path)
     document_features = g.index_all_features_from_data_set(l.features_path)
     model_for_query = g.get_chosen_model_for_queries(c.models_path,c.chosen_models,q.query_to_fold_index)
     queries_budget, competitors, cost_index,first_competitor_features_per_query = g.create_budget_and_costs_for_data(c.final_score_path+"/final_score_combined.txt",10,0.3,model_for_query,document_features)
