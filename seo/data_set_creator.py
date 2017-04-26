95af118864c64b90b423bef7fe68b524d34bd390
gregory goren <grggoren@gmail.com>
2017-04-27 00:59:01 +0300

    data_set_creator addition

--- seo/data_set_creator.py
+++ seo/data_set_creator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     data_set_location = "/lv_local/home/sgregory/letor"
     new_data_set_location = "/lv_local/home/sgregory/letor_fixed1"
     l = lfc.letor_folds_creator(data_set_location,new_data_set_location,True)
-    c = cv.cross_validator(5,l,new_data_set_location)
+    c = cv.cross_validator(5,l,"LTOR1")
     c.k_fold_cross_validation("SVM","qrels")
 
 
