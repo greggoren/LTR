bef9da0dece98c7056157cf79f37778c7f88674b
gregory goren <grggoren@gmail.com>
2017-04-26 20:39:15 +0300

    data_set_creator addition

--- seo/data_set_creator.py
+++ seo/data_set_creator.py
@@ -5,8 +5,8 @@ from model_running import cross_validator as cv
 from seo import exponential_budget_cost_creator as e
 if __name__ == "__main__":
 
-    data_set_location = "C:/study/letor"
-    new_data_set_location = "C:/study/letor_fixed1"
+    data_set_location = "/lv_local/home/sgregory/letor"
+    new_data_set_location = "/lv_local/home/sgregory/letor_fixed1"
     l = lfc.letor_folds_creator(data_set_location,new_data_set_location,False)
     l.split_train_file_into_folds()
 
